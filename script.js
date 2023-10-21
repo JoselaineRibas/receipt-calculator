7d080bac56cce9c5e4aec04fe1568b2bb2af45fb
panayiotisgeorgiou <panayiotisgeorgiou87@gmail.com>
2023-10-21 15:01:19 +0300

    added toggle functionality

--- script.js
+++ script.js
@@ -16,3 +16,18 @@ function calcularRendimento() {
     // Exibir o resultado
     document.getElementById("resultado").innerText = "O valor final após " + tempo + " meses será R$" + valorFinal.toFixed(2);
   }
+
+  function toggleTheme() {
+    const themeStyleLink = document.getElementById("theme-style");
+  
+    // Get the current theme
+    const currentTheme = themeStyleLink.getAttribute("href");
+    console.log('currentTheme - ', currentTheme)
+  
+    // Check the current theme and switch to the opposite theme
+    if (currentTheme === "style-light.css") {
+      themeStyleLink.setAttribute("href", "style-dark.css"); // Apply dark theme
+    } else {
+      themeStyleLink.setAttribute("href", "style-light.css"); // Apply default/light theme
+    }
+  }
